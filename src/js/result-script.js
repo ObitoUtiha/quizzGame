f32dbcd7fab088a2ed25a75d9de261c4db35f582
ObitoUtiha <82911832+ObitoUtiha@users.noreply.github.com>
2023-12-20 12:08:08 +0300

    add result page + css + calc right answers

---
+++ src/js/result-script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var param2 = localStorage.getItem('param2');
+    var param3 = localStorage.getItem('param3');
+    var resultText = document.getElementById('result-text');
+    resultText.textContent+=`: ${param2} of ${param3}`
+
+    var rightProcent = (param2/param3)*100;
+    console.log(rightProcent);
+
+    var img = document.getElementById('result-img');
+    if(rightProcent<=25){
+        img.src = '/src/img/completed25.png'
+    }
+    else if(rightProcent>=80){
+        img.src = '/src/img/completed100.png'
+    }
+    else{
+        img.src = '/src/img/completed50.png'
+    }
+    console.log('Параметр 1:', param2);
+    console.log('Параметр 2:', param3);
+});
